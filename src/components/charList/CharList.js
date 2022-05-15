59011604f84965f7b3073eee705aad4683413d73
Sergey Zlobin <szlobin@lincolnlabs.co>
2022-05-15 10:09:57 +0500

    Update Character List class component to functional component with hooks.

--- src/components/charList/CharList.js
+++ src/components/charList/CharList.js
@@ -1,4 +1,4 @@
-import { Component } from 'react';
+import { useState, useEffect, useRef } from 'react';
 
 import Spinner from '../spinner/Spinner';
 import ErrorMessage from '../errorMessage/ErrorMessage';
@@ -7,101 +7,104 @@ import MarvelService from '../../services/MarvelService';
 
 import './charList.scss';
 
-class CharList extends Component {
-  state = {
-    characterList: [],
-    isLoading: true,
-    isError: false,
-    isAdditionalCharactersLoading: false,
-    additionalCharacherOffset: 0,
-    additionalCharacherOffsetStep: 9,
-    isCharacterListEnded: false,
+const CharList = ({ onCharacterSelected }) => {
+  const _additionalCharacterOffsetStep = 9;
+
+  const [characterList, setCharacterList] = useState([]),
+    [isLoading, setIsLoading] = useState(true),
+    [isError, setIsError] = useState(false),
+    [isAdditionalCharactersLoading, setIsAdditionalCharactersLoading] = useState(false),
+    [additionalCharacterOffset, setAdditionalCharacterOffset] = useState(0),
+    [isCharacterListEnded, setIsCharacterListEnded] = useState(false);
+
+  const marvelService = new MarvelService();
+
+  useEffect(() => {
+    onRequest();
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  const onCharacterListLoaded = (additionalCharacterList) => {
+    setCharacterList(characterList => [...characterList, ...additionalCharacterList]);
+    setIsLoading(false);
+    setIsAdditionalCharactersLoading(false);
+    setAdditionalCharacterOffset(additionalCharacterOffset => additionalCharacterOffset + _additionalCharacterOffsetStep);
+    setIsCharacterListEnded(additionalCharacterList.length < _additionalCharacterOffsetStep ? true : false);
   }
 
-  marvelService = new MarvelService();
-
-  componentDidMount() {
-    this.onRequest();
+  const onError = () => {
+    setIsLoading(false);
+    setIsError(true);
   }
 
-  onCharacterListLoaded = additionalCharacterList => {
-    this.setState(({ characterList, additionalCharacherOffset, additionalCharacherOffsetStep }) => ({
-      characterList: [...characterList, ...additionalCharacterList],
-      isLoading: false,
-      isAdditionalCharactersLoading: false,
-      additionalCharacherOffset: additionalCharacherOffset + additionalCharacherOffsetStep,
-      isCharacterListEnded: additionalCharacterList.length < additionalCharacherOffsetStep ? true : false,
-    }));
+  const onRequest = additionalCharacterOffset => {
+    onAdditionalCharactersLoading();
+
+    marvelService
+      .getAllCharacters(additionalCharacterOffset, _additionalCharacterOffsetStep)
+      .then(onCharacterListLoaded)
+      .catch(onError);
+  };
+
+  const onAdditionalCharactersLoading = () => {
+    setIsAdditionalCharactersLoading(true);
   }
 
-  onError = () => {
-    this.setState({
-      isLoading: false,
-      isError: true,
-    });
+  const itemRefs = useRef([]);
+
+  const focusOnItem = index => {
+    itemRefs.current.forEach(item => item.classList.remove('character__item--selected'));
+    itemRefs.current[index].classList.add('character__item--selected');
+    itemRefs.current[index].focus();
   }
 
-  onRequest = additionalCharacherOffset => {
-    this.onAdditionalCharactersLoading();
-
-    this.marvelService
-      .getAllCharacters(additionalCharacherOffset)
-      .then(this.onCharacterListLoaded)
-      .catch(this.onError);
-  }
-
-  onAdditionalCharactersLoading = () => {
-    this.setState({
-      isAdditionalCharactersLoading: true,
-    })
-  }
-
-  render() {
-    const { characterList, isLoading, isError, isAdditionalCharactersLoading, additionalCharacherOffset, isCharacterListEnded } = this.state;
-    const { onCharacterSelected } = this.props;
-
-    const renderedCharacterList = characterList.length === 0 ? null : (
-      <>
-        <ul className="character__grid">
-          {
-            characterList.map(({ id, name, thumbnail }) => {
-              return (
-                <li className="character__item"
-                  key={id}
-                  onClick={() => onCharacterSelected(id)}>
-                  <CharacterThumbnail thumbnailSrc={thumbnail} thumbnailAlt={name} />
-                  <p className="character__name">{name}</p>
-                </li>
-              )
-            })
-          }
-        </ul>
-
+  const renderedCharacterList = characterList.length === 0 ? null : (
+    <>
+      <ul className="character__grid">
         {
-          !isCharacterListEnded && (
-            <button className="button button__long"
-              disabled={isAdditionalCharactersLoading}
-              onClick={() => this.onRequest(additionalCharacherOffset)}
-            >
-              {isAdditionalCharactersLoading ? <Spinner type='white' size='small' /> : 'Load More'}
-            </button>
-          )
+          characterList.map(({ id, name, thumbnail }, index) => {
+            return (
+              <li className="character__item"
+                key={id}
+                tabIndex={0}
+                ref={element => itemRefs.current[index] = element}
+                onClick={() => {
+                  onCharacterSelected(id);
+                  focusOnItem(index);
+                }}>
+                <CharacterThumbnail thumbnailSrc={thumbnail} thumbnailAlt={name} />
+                <p className="character__name">{name}</p>
+              </li>
+            )
+          })
         }
-      </>
-    );
+      </ul>
 
-    const spinner = isLoading && <Spinner />;
-    const errorMessage = isError && <ErrorMessage message='Failed to load random character.' />;
-    const content = !(isLoading || isError) && renderedCharacterList;
+      {
+        !isCharacterListEnded && (
+          <button className="button button__long"
+            disabled={isAdditionalCharactersLoading}
+            onClick={() => onRequest(additionalCharacterOffset)}
+          >
+            {isAdditionalCharactersLoading ? <Spinner type='white' size='small' /> : 'Load More'}
+          </button>
+        )
+      }
+    </>
+  );
 
-    return (
-      <div className="character__list">
-        {spinner}
-        {errorMessage}
-        {content}
-      </div>
-    );
-  }
+  const spinner = isLoading && <Spinner />;
+  const errorMessage = isError && <ErrorMessage message='Failed to load random character.' />;
+  const content = !(isLoading || isError) && renderedCharacterList;
+
+  return (
+    <div className="character__list">
+      {spinner}
+      {errorMessage}
+      {content}
+    </div>
+  );
 }
 
 // TODO: Divide to another file.
